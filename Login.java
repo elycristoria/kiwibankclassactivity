3cb27dcd386e709d794d9609f70e13c380498102
Rois <rois.climaco@gmail.com>
2020-02-19 17:54:26 +1300

    Initial PUsh

---
+++ Login.java
@@ -0,0 +1,8 @@
+public class Login
+{
+	public static void main(String[] args){
+		System.out.println("Welcome User to Kiwibank!");
+		System.out.println("Thanks for Visiting!");
+		System.out.println("- RC");
+	}
+}
