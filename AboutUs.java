40e2ed5025f92bdcea92a88e4545bfe3e0e3aa88
Fang <jackie@fang.co.nz>
2020-02-21 09:50:11 +1300

    fang push

---
+++ AboutUs.java
@@ -0,0 +1,12 @@
+public class Login
+{
+	public static void main(String[] args){
+		System.out.println("Welcome User to ASB!");
+		System.out.println("@_@About US@_@");
+                                System.out.println("RBNZ");
+<<<<<<< HEAD
+              System.out.println("New Line");
+=======
+>>>>>>> 95776d8463d279ae7f048b8fe85d4a1a9805931c
+	}
+}
